d44c2ab7a138e10460be9776056a04f62f7da986
archeryue <archeryue7@gmail.com>
2025-04-05 17:18:04 +0800

    ADD: S02E01: learn from micrograd and write the tinyflow

---
+++ Season-2/tinyflow/__init__.py
@@ -0,0 +1,5 @@
+# copy from Karpathy/micrograd for pedagogical purpose
+
+from .autograd import Scalar
+
+__all__ = ['Scalar']
---
+++ Season-2/tinyflow/autograd.py
@@ -0,0 +1,80 @@
+# auto-grad engine for scalars
+
+class Scalar:
+    def __init__(self, val, _prev=(), _op=''):
+        self.val = val
+        self.grad = 0
+        # attributes for auto-grad
+        self._backward = lambda: None
+        self._prev = set(_prev)
+        self._op = _op
+
+    def __repr__(self):
+        return f"Scalar(val={self.val}, grad={self.grad})"
+
+    def __add__(self, other):
+        next = Scalar(self.val + other.val, (self, other), '+')
+        def _backward():
+            self.grad += next.grad
+            other.grad += next.grad
+        next._backward = _backward
+        return next
+
+    def __mul__(self, other):
+        next = Scalar(self.val * other.val, (self, other), '*')
+        def _backward():
+            self.grad += next.grad * other.val
+            other.grad += next.grad * self.val
+        next._backward = _backward
+        return next
+
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Wrong Operand Type"
+        next = Scalar(self.val ** other, (self,), f'**{other}')
+        def _backward():
+            self.grad += next.grad * other * self.val ** (other - 1)
+        next._backward = _backward
+        return next
+    
+    def relu(self):
+        next = Scalar(max(0, self.val), (self,), 'ReLU')
+        def _backward():
+            self.grad += next.grad * (self.val > 0)
+        next._backward = _backward
+        return next
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for p in v._prev:
+                    build_topo(p)
+                topo.append(v)
+        build_topo(self)
+        # back-propagate the gradients
+        self.grad = 1.0
+        for v in reversed(topo):
+            v._backward()
+
+    def __neg__(self):
+        return self * -1
+
+    def __radd__(self, other):
+        return self + other
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __sub__(self, other):
+        return self + (-other)
+
+    def __rsub__(self, other):
+        return other + (-self)
+    
+    def __truediv__(self, other):
+        return self * (other ** -1)
+    
+    def __rtruediv__(self, other):
+        return other * (self ** -1)
